b3e6b5f3a392d74232f4b3a4a693e5a086a44301
soumya773 <150830999+soumya773@users.noreply.github.com>
2025-04-30 12:55:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-const priceElements = document.querySelectorAll(".price");
+const getSumBtn = document.createElement("button");
+getSumBtn.append("Get Total Price");
+document.body.appendChild(getSumBtn);
+
+const getSum = () => {
+//Add your code here
+   const getSum = () => {
+  const prices = document.querySelectorAll(".price");
   let total = 0;
 
-  priceElements.forEach(price => {
-    total += parseFloat(price.textContent) || 0;
+  prices.forEach(cell => {
+    const val = parseFloat(cell.textContent) || 0;
+    total += val;
   });
 
-  const resultDiv = document.getElementById("ans");
-  resultDiv.textContent = total;
+  const table = document.querySelector("table");
+  const totalRow = document.createElement("tr");
+  const totalCell = document.createElement("td");
+  totalCell.setAttribute("colspan", "2");
+  totalCell.setAttribute("id", "ans"); // So Cypress can find it
+  totalCell.textContent = total;
+
+  totalRow.appendChild(totalCell);
+  table.appendChild(totalRow);
+};
+
+};
+
+getSumBtn.addEventListener("click", getSum);
+
