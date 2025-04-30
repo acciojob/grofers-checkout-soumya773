2f8a9658e82a40605f840b788c3775c98939fad0
soumya773 <150830999+soumya773@users.noreply.github.com>
2025-04-30 12:46:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+   const priceElements = document.querySelectorAll(".prices");
+  let total = 0;
+
+  priceElements.forEach(price => {
+    total += parseFloat(price.textContent);
 };
 
 getSumBtn.addEventListener("click", getSum);
